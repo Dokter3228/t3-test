c7329e81ed582dd3fb7a21ddcedfa3db508c49da
Dokter322 <Dokter322@yandex.ru>
2023-07-22 01:41:18 +0300

    fix google bug

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import React, { type PropsWithChildren } from "react";
 import { SignInButton, SignOutButton } from "@clerk/nextjs";
 
-const PageLayout = (props: PropsWithChildren<{}>) => {
+const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
       <div className="h-full w-full border-x border-slate-400 md:max-w-3xl ">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { api, type RouterOutputs } from "~/utils/api";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
