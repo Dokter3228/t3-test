407574f8cd12efde0cd2602cf79741fc59241504
Dokter322 <Dokter322@yandex.ru>
2023-07-22 00:06:29 +0300

    add working posts and invalidation

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs";
 import { type User } from "@clerk/backend";
 import { type RouterOutputs } from "~/utils/api";
@@ -17,6 +21,7 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: { createdAt: "desc" },
     });
 
     const users = (
@@ -25,8 +30,6 @@ export const postsRouter = createTRPCRouter({
         limit: 100,
       })
     ).map(filetUserForClient);
-
-    console.log(users);
 
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
@@ -45,4 +48,18 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().min(1).max(200),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      return await ctx.prisma.post.create({
+        data: { authorId, content: input.content },
+      });
+    }),
 });
